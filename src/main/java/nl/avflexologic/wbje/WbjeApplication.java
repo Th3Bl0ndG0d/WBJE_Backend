b02a69467707b97b756b731089d950d47bfbbd88
Th3Bl0ndG0d <michel_karsten@hotmail.com>
2025-12-22 11:16:20 +0100

    Initial commit

---
+++ src/main/java/nl/avflexologic/wbje/WbjeApplication.java
@@ -0,0 +1,13 @@
+package nl.avflexologic.wbje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbjeApplication.class, args);
+    }
+
+}
